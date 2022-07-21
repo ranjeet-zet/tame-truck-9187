c06b98745dddabee8388771a84e8da2be8569f12
sunidhi-chauhan <sunidhichauhan780@gmail.com>
2022-07-21 19:31:36 +0530

    payment js part done almost

--- checkout_Page/script/checkout.js
+++ checkout_Page/script/checkout.js
@@ -11,15 +11,34 @@ function next() {
         name: formdata.first_name.value,
         lastname: formdata.last_name.value,
         country: formdata.country.value,
-        billing: formdata.Billing_email.value,
-        cardName: document.getElementById("payment2_card").value,
-        cardInfo: document.getElementById("payment3_card").value
+        billing: formdata.Billing_email.value
+        // cardName: document.getElementById("payment2_card").value,
+        // cardInfo: document.getElementById("payment3_card").value
 
     }
     localStorage.setItem("details", JSON.stringify(obj))
-    console.log(obj.name, obj.lastname, obj.country, obj.billing, obj.cardName)
+    // console.log(obj.name, obj.lastname, obj.country, obj.billing, obj.cardName)
 
 
+    var x = document.getElementById("user_Details");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        console.log("start")
+    } else {
+        x.style.display = "none";
+        // console.log("end")
+        document.getElementById("payment").style.marginTop = "-500px";
+    }
+
+}
 
 
+function show() {
+    var x = document.getElementById("user_Details");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("payment").style.marginTop = "500px";
+    } else {
+        x.style.display = "none";
+    }
 }
